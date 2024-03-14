c9fbf0ee3f844dafae43a0621f35b849756afaa2
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-03-14 10:22:02 -0300

    refactor onboarding layout

--- src/components/layout/OnboardingLayout.tsx
+++ src/components/layout/OnboardingLayout.tsx
@@ -4,34 +4,73 @@ import CssBaseline from '@mui/material/CssBaseline'
 import Providers from '@/providers/Providers'
 import LogoType from '../LogoType'
 import Footer from '../template/Footer'
+import Container from '@mui/material/Container'
 
 const Header = () => {
-	return (
-		<AppBar
-			sx={{
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'flex-end',
-				height: 120,
-				p: 2
-			}}
-		>
-			<LogoType />
-		</AppBar>
-	)
+  return (
+    <AppBar
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        height: 120,
+        p: 2,
+      }}
+    >
+      <LogoType />
+    </AppBar>
+  )
 }
 
-const OnboardingLayout = ({children}: {children: React.ReactNode}) => {
-	return (
-		<Providers>
-			<CssBaseline />
-			<Header />
-			<Box component="main" sx={{marginTop: '120px'}}>
-				{children}
-			</Box>
-			<Footer />
-		</Providers>
-	)
+const FullSection = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Box
+      component="section"
+      sx={{
+        minHeight: 'calc(100vh - 120px)',
+        maxWidth: '600px',
+        margin: '0 auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        p: 0,
+        m: 0,
+      }}
+    >
+      <Container
+        sx={{
+          py: 7.5,
+          px: 3,
+          height: '100%',
+        }}
+      >
+        {children}
+      </Container>
+    </Box>
+  )
+}
+
+const OnboardingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Providers>
+      <CssBaseline />
+      <Header />
+      <Box
+        component="main"
+        sx={{
+          marginTop: '120px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <FullSection>{children}</FullSection>
+      </Box>
+      <Footer />
+    </Providers>
+  )
 }
 
 export default OnboardingLayout
