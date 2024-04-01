e5d301711a4df5c5be4ddfba01b5e6fe86e0e150
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-04-01 10:13:52 -0300

    apply eslint formatter

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,12 +1,12 @@
 import ColorModeProvider from './ColorModeProvider'
 import ThemeProvider from './ThemeProvider'
 
-const Providers = ({children}: {children?: React.ReactNode}) => {
-	return (
-		<ColorModeProvider>
-			<ThemeProvider>{children}</ThemeProvider>
-		</ColorModeProvider>
-	)
+const Providers = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <ColorModeProvider>
+      <ThemeProvider>{children}</ThemeProvider>
+    </ColorModeProvider>
+  )
 }
 
 export default Providers
--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,12 +1,12 @@
-import { ThemeProvider as Theme } from "@mui/material/styles";
-import { light as lightTheme } from "../themes/light";
-import { dark as darkTheme } from "../themes/dark";
-import useColorMode from "../hooks/useColorMode";
+import { ThemeProvider as Theme } from '@mui/material/styles'
+import { light as lightTheme } from '../themes/light'
+// import { dark as darkTheme } from '../themes/dark'
+// import useColorMode from '../hooks/useColorMode'
 
 const ThemeProvider = ({ children }: { children?: React.ReactNode }) => {
-  const mode = useColorMode();
+  // const mode = useColorMode()
 
-  return <Theme theme={lightTheme}>{children}</Theme>;
-};
+  return <Theme theme={lightTheme}>{children}</Theme>
+}
 
-export default ThemeProvider;
+export default ThemeProvider
