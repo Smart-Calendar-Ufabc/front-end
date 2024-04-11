1c812cfef8dd3c8482698e5aaf29f7822e8ec59f
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-04-10 21:11:44 -0300

    Update LogoType component in LandingPageLayout.tsx

--- src/components/layout/LandingPageLayout.tsx
+++ src/components/layout/LandingPageLayout.tsx
@@ -41,7 +41,7 @@ const Header = () => {
       })}
     >
       <Box>
-        <LogoType direction="vertical" />
+        <LogoType />
       </Box>
       <MobileUp>
         <Box sx={{ display: 'flex', gap: 2 }}>
