eb3c9f7f6a942b670ea37b54c1b914b3c5a0a87e
Ana Monici <ana.monici@outlook.com>
2024-04-16 17:19:36 -0300

    Updated DialogEditTask.tsx

--- src/components/dialogs/DialogEditTask.tsx
+++ src/components/dialogs/DialogEditTask.tsx
@@ -5,7 +5,27 @@ import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
 import IconButton from '@mui/material/IconButton'
 import Divider from '@mui/material/Divider'
-import { X as CloseIcon } from '@phosphor-icons/react'
+import { X as CloseIcon, Trash as TrashIcon, ClockClockwise as ClockIcon, Columns } from '@phosphor-icons/react'
+import {
+  FormGroup,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  InputLabel,
+  Select,
+  MenuItem,
+  SvgIcon
+} from '@mui/material'
+import { Dayjs } from 'dayjs'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import { FormHelperTag } from '../typography/FormHelperTag'
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import { DatePicker, TimePicker } from '@mui/x-date-pickers'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import 'dayjs/locale/pt-br'
 
 interface DialogEditTaskProps {
   open: boolean
@@ -13,6 +33,45 @@ interface DialogEditTaskProps {
 }
 
 export function DialogEditTask({ open, onClose }: DialogEditTaskProps) {
+  const validationSchema = yup.object({
+    title: yup.string().required('Título é obrigatório'),
+    notes: yup.string(),
+    priority: yup.string().required('Prioridade é obrigatória'),
+    duration: yup.string().required('Duração é obrigatória'),
+    dueDate: yup.date().required('Data é obrigatória'),
+    dueTime: yup.date().required('Hora é obrigatória'),
+    startDate: yup.date().nullable(),
+    startTime: yup.date().nullable()
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      notes: '',
+      priority: '',
+      duration: null,
+      dueDate: null,
+      dueTime: null,
+      startDate: null,
+      startTime: null,
+    } as {
+      title: string
+      notes: string
+      priority: 'low' | 'medium' | 'high' | ''
+      duration: Dayjs | null
+      dueDate: Dayjs | null
+      dueTime: Dayjs | null
+      startDate: Dayjs | null,
+      startTime: Dayjs | null,
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      // formik.resetForm()
+
+      // onClose()
+    },
+  })
+
   return (
     <Dialog
       open={open}
@@ -37,7 +96,308 @@ export function DialogEditTask({ open, onClose }: DialogEditTaskProps) {
         <CloseIcon />
       </IconButton>
       <Divider />
-      <DialogContent>CRIE AQUI...</DialogContent>
+      <DialogContent>
+        <Stack direction="column" spacing={3}>
+          {/* Título */}
+          <TextField
+            name="title"
+            label="Título"
+            type="text"
+            size="small"
+            id="outlined-start-adornment"
+            sx={{ m: 1, width: 'Fill' }}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.title}
+            error={formik.touched.title && Boolean(formik.errors.title)}
+            helperText={formik.touched.title && formik.errors.title}
+          />
+          {/* Notas */}
+          <TextField
+            name="notes"
+            multiline
+            rows={4}
+            label="Notas"
+            type="text"
+            id="outlined-multiline-static"
+            sx={{ m: 1, width: 'Fill' }}
+            defaultValue="Default Value"
+            value={formik.values.notes}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.notes && Boolean(formik.errors.notes)}
+            helperText={
+              formik.touched.notes && Boolean(formik.errors.notes) ? (
+                formik.touched.notes && formik.errors.notes
+              ) : (
+                <FormHelperTag>Opcional</FormHelperTag>
+              )
+            }
+          />
+          {/* Duração */}
+          <LocalizationProvider
+            dateAdapter={AdapterDayjs}
+            adapterLocale="pt-br"
+          >
+            <FormControl size="small">
+              <TimePicker
+                name="duration"
+                label="Duração"
+                ampm={false}
+                slotProps={{
+                  textField: {
+                    name: 'duration',
+                    size: 'small',
+                    type: 'text',
+                    variant: 'outlined',
+                    error:
+                      formik.touched.duration &&
+                      Boolean(formik.errors.duration),
+                  },
+                }}
+                value={formik.values.duration}
+                onChange={(newValue) => {
+                  formik.setFieldValue('duration', newValue)
+                }}
+              />
+              <FormHelperText
+                error={
+                  formik.touched.duration && Boolean(formik.errors.duration)
+                }
+              >
+                {formik.touched.duration && formik.errors.duration}
+              </FormHelperText>
+            </FormControl>
+          </LocalizationProvider>
+          {/* Data de inicio */}
+          <FormGroup>
+            <FormControl>
+              <FormLabel component="legend">Data de início</FormLabel>
+              <FormGroup>
+                <Stack direction="row" spacing={2} mt={2}>
+                  <FormControl sx={{ flex: 2 }}>
+                    <LocalizationProvider
+                      dateAdapter={AdapterDayjs}
+                      adapterLocale="pt-br"
+                    >
+                      <DatePicker
+                        name="startDate"
+                        label="Data"
+                        slotProps={{
+                          textField: {
+                            name: 'startDate',
+                            size: 'small',
+                            type: 'text',
+                            variant: 'outlined',
+                            error:
+                              formik.touched.startDate &&
+                              Boolean(formik.errors.startDate),
+                          },
+                        }}
+                        value={formik.values.startDate}
+                        onChange={(newValue) => {
+                          formik.setFieldValue('startDate', newValue)
+                        }}
+                      />
+                      <FormHelperText
+                        error={
+                          formik.touched.startDate && Boolean(formik.errors.startDate)
+                        }
+                      >
+                        {formik.touched.startDate && formik.errors.startDate}
+                      </FormHelperText>
+                    </LocalizationProvider>
+                  </FormControl>
+                  <FormControl sx={{ flex: 1 }}>
+                    <LocalizationProvider
+                      dateAdapter={AdapterDayjs}
+                      adapterLocale="pt-br"
+                    >
+                      <TimePicker
+                        name="startTime"
+                        label="Hora"
+                        ampm={false}
+                        slotProps={{
+                          textField: {
+                            name: 'startTime',
+                            size: 'small',
+                            type: 'text',
+                            variant: 'outlined',
+                            error:
+                              formik.touched.startTime &&
+                              Boolean(formik.errors.startTime),
+                          },
+                        }}
+                        value={formik.values.startTime}
+                        onChange={(newValue) => {
+                          formik.setFieldValue('startTime', newValue)
+                        }}
+                      />
+                      <FormHelperText
+                        error={
+                          formik.touched.startTime && Boolean(formik.errors.startTime)
+                        }
+                      >
+                        {formik.touched.startTime && formik.errors.startTime}
+                      </FormHelperText>
+                    </LocalizationProvider>
+                  </FormControl>
+                </Stack>
+              </FormGroup>
+            </FormControl>
+          </FormGroup>
+          {/* Data limite de entrega */}
+          <FormGroup>
+            <FormControl>
+              <FormLabel component="legend">Data limite de entrega</FormLabel>
+              <FormGroup>
+                <Stack direction="row" spacing={2} mt={2}>
+                  <FormControl sx={{ flex: 2 }}>
+                    <LocalizationProvider
+                      dateAdapter={AdapterDayjs}
+                      adapterLocale="pt-br"
+                    >
+                      <DatePicker
+                        name="dueDate"
+                        label="Data"
+                        slotProps={{
+                          textField: {
+                            name: 'dueDate',
+                            size: 'small',
+                            type: 'text',
+                            variant: 'outlined',
+                            error:
+                              formik.touched.dueDate &&
+                              Boolean(formik.errors.dueDate),
+                          },
+                        }}
+                        value={formik.values.dueDate}
+                        onChange={(newValue) => {
+                          formik.setFieldValue('dueDate', newValue)
+                        }}
+                      />
+                      <FormHelperText
+                        error={
+                          formik.touched.dueDate && Boolean(formik.errors.dueDate)
+                        }
+                      >
+                        {formik.touched.dueDate && formik.errors.dueDate}
+                      </FormHelperText>
+                    </LocalizationProvider>
+                  </FormControl>
+                  <FormControl sx={{ flex: 1 }}>
+                    <LocalizationProvider
+                      dateAdapter={AdapterDayjs}
+                      adapterLocale="pt-br"
+                    >
+                      <TimePicker
+                        name="dueTime"
+                        label="Hora"
+                        ampm={false}
+                        slotProps={{
+                          textField: {
+                            name: 'dueTime',
+                            size: 'small',
+                            type: 'text',
+                            variant: 'outlined',
+                            error:
+                              formik.touched.dueTime &&
+                              Boolean(formik.errors.dueTime),
+                          },
+                        }}
+                        value={formik.values.dueTime}
+                        onChange={(newValue) => {
+                          formik.setFieldValue('dueTime', newValue)
+                        }}
+                      />
+                      <FormHelperText
+                        error={
+                          formik.touched.dueTime && Boolean(formik.errors.dueTime)
+                        }
+                      >
+                        {formik.touched.dueTime && formik.errors.dueTime}
+                      </FormHelperText>
+                    </LocalizationProvider>
+                  </FormControl>
+                </Stack>
+              </FormGroup>
+            </FormControl>
+          </FormGroup>
+          {/* Prioridade */}
+          <FormControl size="small">
+            <InputLabel id="priority-select-label">Prioridade</InputLabel>
+            <Select
+              name="priority"
+              labelId="priority-select-label"
+              id="priority-select"
+              label="Prioridade"
+              value={formik.values.priority}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.priority && Boolean(formik.errors.priority)}
+            >
+              <MenuItem value="low">Baixa</MenuItem>
+              <MenuItem value="medium">Média</MenuItem>
+              <MenuItem value="high">Alta</MenuItem>
+            </Select>
+            <FormHelperText
+              error={formik.touched.priority && Boolean(formik.errors.priority)}
+            >
+              {formik.touched.priority && formik.errors.priority}
+            </FormHelperText>
+          </FormControl>
+          {/* Botões */}
+          <Stack direction="row" spacing={2} sx={{bgcolor: 'grey.100', width: 'Fill', placeContent: 'center', justifyContent: 'space-evenly'}}>
+            {/* Opção Excluir */}
+            <Button
+              sx={(theme) => ({
+                py: '10px',
+                [theme.breakpoints.down('sm')]: {
+                  width: '100%',
+                },
+                color: 'grey.600',
+                '&:hover': {
+                  color: 'error.main',
+                  backgroundColor: 'grey.100',
+                },
+                flexDirection: 'column'
+              })}
+            >
+              <SvgIcon
+                sx={({
+                  marginBottom: '12px'
+                })}
+              >
+                <TrashIcon />
+              </SvgIcon>
+              Excluir
+            </Button>
+            {/* Opção Reagendar */}
+            <Button
+              sx={(theme) => ({
+                py: '10px',
+                [theme.breakpoints.down('sm')]: {
+                  width: '100%',
+                },
+                color: 'grey.600',
+                '&:hover': {
+                  color: 'primary.main',
+                },
+                flexDirection: 'column'
+              })}
+            >
+              <SvgIcon
+                sx={({
+                  marginBottom: '12px'
+                })}
+              >
+                <ClockIcon />
+              </SvgIcon>
+              Reagendar
+            </Button>
+          </Stack>
+        </Stack>
+      </DialogContent>
       <DialogActions>
         <Button
           variant="contained"
@@ -45,6 +405,7 @@ export function DialogEditTask({ open, onClose }: DialogEditTaskProps) {
           sx={{
             py: '10px',
           }}
+          onClick={formik.submitForm}
         >
           Salvar Alterações
         </Button>
