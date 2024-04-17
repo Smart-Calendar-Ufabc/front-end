a8e31a5faa8d304f665ea4f6314bec13b0605b18
Thais Rodrigues <thaiskmr3@gmail.com>
2024-04-17 01:53:33 -0300

    Pagina de recuperacao de senha

--- src/app/password-recovery/page.tsx
+++ src/app/password-recovery/page.tsx
@@ -1,20 +1,91 @@
 'use client'
 
+import { useState } from 'react'
+import { useFormik } from 'formik'
+import FormContainer from '@/components/form/FormContainer'
+import FormGroup from '@/components/form/FormGroup'
+import FormTitle from '@/components/form/FormTitle'
 import OnboardingLayout from '@/components/layout/OnboardingLayout'
-import PageUnderConstruction from '@/components/PageUnderConstructionAlert'
 import { useAppStates } from '@/store/useAppStates'
-import { redirect } from 'next/navigation'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import { signUpFetch } from '../api/sign-up'
+import { Alert, CircularProgress } from '@mui/material'
 
-export default function PasswordRecovery() {
-  const authToken = useAppStates.getState().authToken
+export default function SignUp() {
+  const [openAlert, setOpenAlert] = useState(false)
+  const [alertMessage, setAlertMessage] = useState('')
+  const { setOnboarding } = useAppStates()
+  const [isLoading, setIsLoading] = useState(false)
 
-  if (authToken) {
-    redirect('/home')
-  }
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      
+    },
+    onSubmit: async (values) => {
+      setIsLoading(true)
+      const { data, status } = await signUpFetch(values)
+
+      if (status === 200) {
+        setOpenAlert(true)
+        setAlertMessage('Um link para redefinição de senha foi enviado para o seu e-mail.')
+        formik.resetForm()
+      } else if (status === 404) {
+        setIsLoading(false)
+        formik.setErrors({
+          email: 'Email não encontrado. Verifique se digitou corretamente.',
+        })
+      } else if (status === 500) {
+        setIsLoading(false)
+        setOpenAlert(true)
+        setAlertMessage('Erro interno no servidor.')
+      }
+    },
+  })
 
   return (
     <OnboardingLayout>
-      <PageUnderConstruction title="Recuperar Conta" />
+      <FormContainer>
+        <FormTitle>Recuperar Conta</FormTitle>
+        {openAlert && (
+          <Alert
+            severity="error"
+            onClose={() => {
+              setOpenAlert(false)
+            }}
+          >
+            {alertMessage}
+          </Alert>
+        )}
+        <Typography>
+            Insira seu email para receber um código de confirmação e recuperar sua conta.
+          </Typography>
+        <FormGroup>
+          <FormGroup>
+            <TextField
+              name="email"
+              label="E-mail"
+              type="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+          </FormGroup>
+          
+        </FormGroup>
+        <Button
+          variant="contained"
+          onClick={formik.submitForm}
+          disabled={isLoading}
+        >
+          {isLoading ? <CircularProgress size={16} /> : 'Enviar Código'}
+        </Button>
+      </FormContainer>
     </OnboardingLayout>
   )
 }
