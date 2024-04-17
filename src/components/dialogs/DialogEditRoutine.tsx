caeac8bd63b0438af8eee8f17e7a3ed54cb42481
ArthurRosin <arthur.rosin@aluno.ufabc.edu.br>
2024-04-17 09:25:49 -0300

    ValidationEditRoutine

--- src/components/dialogs/DialogEditRoutine.tsx
+++ src/components/dialogs/DialogEditRoutine.tsx
@@ -5,50 +5,438 @@ import DialogContent from '@mui/material/DialogContent'
 import DialogTitle from '@mui/material/DialogTitle'
 import IconButton from '@mui/material/IconButton'
 import Divider from '@mui/material/Divider'
+import MuiCheckbox, { CheckboxProps } from '@mui/material/Checkbox'
 import { X as CloseIcon } from '@phosphor-icons/react'
+import { TimePicker } from '@mui/x-date-pickers/TimePicker'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import React, { useState } from 'react'
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  FormHelperText,
+  FormLabel,
+  FormGroup,
+  FormControlLabel,
+  Box,
+} from '@mui/material'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import 'dayjs/locale/pt-br'
 
-interface DialogEditRoutineProps {
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import dayjs, { Dayjs } from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import isBetween from 'dayjs/plugin/isBetween'
+import { useSchedulesStates } from '@/store/useSchedulesStates'
+import './styles.css'
+import { Schedule } from '@/entities/Schedule'
+import AlertDialog from './AlertDialog'
+import { FormHelperTag } from '../typography/FormHelperTag'
+
+dayjs.extend(utc)
+dayjs.extend(isBetween)
+
+interface DialogAddRoutineProps {
   open: boolean
   onClose: () => void
 }
 
-export function DialogEditRoutine({ open, onClose }: DialogEditRoutineProps) {
+const isThereConflict = (schedule: Schedule, newSchedule: Schedule) => {
+  const scheduleStartDate = dayjs(schedule.startAt).second(0)
+  const scheduleEndDate = dayjs(schedule.endAt).second(0)
+
+  const newScheduleStartDate = dayjs(newSchedule.startAt)
+  const newScheduleEndDate = dayjs(newSchedule.endAt)
+
+  const newScheduleStartDateIsBetween = newScheduleStartDate.isBetween(
+    scheduleStartDate,
+    scheduleEndDate,
+    null,
+    '[]',
+  )
+
+  const newScheduleEndDateIsBetween = newScheduleEndDate.isBetween(
+    scheduleStartDate,
+    scheduleEndDate,
+    null,
+    '[]',
+  )
+
+  const newScheduleInSame =
+    newScheduleStartDate.isSame(scheduleStartDate) ||
+    newScheduleStartDate.isSame(scheduleEndDate) ||
+    newScheduleEndDate.isSame(scheduleStartDate) ||
+    newScheduleEndDate.isSame(scheduleEndDate)
+
   return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      sx={{
-        '& .MuiDialog-paper': {
-          minWidth: 500,
-        },
-      }}
-    >
-      <DialogTitle sx={{ m: 0, p: 2 }}>Editar Rotina</DialogTitle>
-      <IconButton
-        aria-label="close"
-        onClick={onClose}
-        sx={{
-          position: 'absolute',
-          right: 8,
-          top: 8,
-          color: (theme) => theme.palette.grey[500],
-        }}
+    newScheduleStartDateIsBetween ||
+    newScheduleEndDateIsBetween ||
+    newScheduleInSame
+  )
+}
+
+export function DialogAddRoutine({ open, onClose }: DialogAddRoutineProps) {
+  const [repeatWeekdays, setRepeatWeekdays] = useState({
+    sunday: false,
+    monday: false,
+    tuesday: false,
+    wednesday: false,
+    thursday: false,
+    friday: false,
+    saturday: false,
+  })
+
+  const { sunday, monday, tuesday, wednesday, thursday, friday, saturday } =
+    repeatWeekdays
+
+  const { schedules, addSchedule } = useSchedulesStates()
+  const [datesConflict, setDatesConflict] = useState<string[]>([])
+  const [openAlertDialog, setOpenAlertDialog] = useState(false)
+  const [alertDialogTitle, setAlertDialogTitle] = useState('')
+  const [alertDialogMessage, setAlertDialogMessage] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRepeatWeekdays((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.checked,
+    }))
+  }
+
+  const validationSchema = yup.object({
+    title: yup.string().required('Informe um título'),
+    startTime: yup.string().required('Informe o horário de início'),
+    endTime: yup.string().required('Informe o horário de término'),
+    repeatAs: yup.string().required('Informe o período de repetição'),
+  })
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      startTime: null,
+      endTime: null,
+      repeatAs: '',
+    } as {
+      title: string
+      startTime: Dayjs | null
+      endTime: Dayjs | null
+      repeatAs: string
+    },
+    validationSchema,
+    onSubmit: ({ endTime, repeatAs, startTime, title }) => {
+      let newSchedules: Schedule[] = []
+      const datesConflicting: string[] = []
+
+      if (repeatAs === 'daily') {
+        newSchedules = Array.from({ length: 30 }).map((_, index) => {
+          const routineStartDate = dayjs()
+            .utc()
+            .add(index, 'day')
+            .hour(startTime?.hour() || 0)
+            .minute(startTime?.minute() || 0)
+            .second(0)
+
+          const routineEndDate = dayjs()
+            .utc()
+            .add(index, 'day')
+            .hour(endTime?.hour() || 0)
+            .minute(endTime?.minute() || 0)
+            .second(0)
+
+          return {
+            id: crypto.randomUUID(),
+            title,
+            status: 'pending',
+            priority: 'routine',
+            startAt: routineStartDate.toDate(),
+            endAt: routineEndDate.toDate(),
+          }
+        })
+
+        newSchedules.map((newSchedule) => {
+          const isConflicting = schedules.some((schedule) =>
+            isThereConflict(schedule, newSchedule),
+          )
+
+          if (!isConflicting) {
+            addSchedule(newSchedule)
+          } else {
+            datesConflicting.push(newSchedule.startAt.toString())
+          }
+
+          return newSchedule
+        })
+      }
+
+      if (repeatAs === 'weekly') {
+        for (let week = 0; week < 4; week++) {
+          Object.values(repeatWeekdays).forEach((isRepeat, index) => {
+            if (isRepeat) {
+              index -= 1
+              const routineStartDate = dayjs()
+                .utc()
+                .add(index + week * 7 - 1, 'day')
+                .hour(startTime?.hour() || 0)
+                .minute(startTime?.minute() || 0)
+                .second(0)
+
+              const routineEndDate = dayjs()
+                .utc()
+                .add(index + week * 7 - 1, 'day')
+                .hour(endTime?.hour() || 0)
+                .minute(endTime?.minute() || 0)
+                .second(0)
+
+              newSchedules.push({
+                id: crypto.randomUUID(),
+                title,
+                status: 'pending',
+                priority: 'routine',
+                startAt: routineStartDate.toDate(),
+                endAt: routineEndDate.toDate(),
+              })
+            }
+          })
+        }
+
+        newSchedules.map((newSchedule) => {
+          const isConflicting = schedules.some((schedule) =>
+            isThereConflict(schedule, newSchedule),
+          )
+
+          if (!isConflicting) {
+            addSchedule(newSchedule)
+          } else {
+            datesConflicting.push(newSchedule.startAt.toString())
+          }
+
+          return newSchedule
+        })
+      }
+
+      if (datesConflicting.length) {
+        setDatesConflict(datesConflicting)
+        handleNotifyConflict()
+      }
+
+      formik.resetForm()
+
+      onClose()
+    },
+  })
+
+  const handleClose = () => {
+    formik.resetForm()
+    onClose()
+  }
+
+  const handleNotifyConflict = () => {
+    setAlertDialogTitle('Conflito de horário.')
+    setAlertDialogMessage(
+      `Não foi possível adicionar esta rotina nos dias listados abaixo pois há conflito de horário com outro agendamento.`,
+    )
+    setOpenAlertDialog(true)
+  }
+
+  return (
+    <>
+      <AlertDialog
+        open={openAlertDialog}
+        onClose={() => setOpenAlertDialog(false)}
+        onConfirm={() => setOpenAlertDialog(false)}
+        title={alertDialogTitle}
+        message={alertDialogMessage}
+        confirmText="Ok"
+        cancelText={false}
       >
-        <CloseIcon />
-      </IconButton>
-      <Divider />
-      <DialogContent>CRIE AQUI...</DialogContent>
-      <DialogActions>
-        <Button
-          variant="contained"
-          fullWidth
+        <Box
           sx={{
-            py: '10px',
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: 1,
+            mt: 2,
           }}
         >
-          Salvar Alterações
-        </Button>
-      </DialogActions>
-    </Dialog>
+          {datesConflict.map((date) => (
+            <FormHelperTag key={date}>
+              {dayjs(date).utc().format('DD/MM/YYYY')}
+            </FormHelperTag>
+          ))}
+        </Box>
+      </AlertDialog>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        sx={(theme) => ({
+          '& .MuiDialog-paper': {
+            width: 400,
+            [theme.breakpoints.down('sm')]: {
+              width: '100%',
+              minWidth: '100%',
+              minHeight: '100%',
+              borderRadius: 0,
+            },
+          },
+        })}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }}>Adicionar Rotina</DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <Divider />
+        <DialogContent>
+          <Stack direction="column" spacing={3}>
+            <TextField
+              name="title"
+              label="Título"
+              type="text"
+              size="small"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.title}
+              error={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
+            />
+            <LocalizationProvider
+              dateAdapter={AdapterDayjs}
+              adapterLocale="pt-br"
+            >
+              <FormControl size="small">
+                <TimePicker
+                  name="startTime"
+                  label="Inicia às"
+                  ampm={false}
+                  slotProps={{
+                    textField: {
+                      name: 'startTime',
+                      size: 'small',
+                      type: 'text',
+                      variant: 'outlined',
+                      error:
+                        formik.touched.startTime &&
+                        Boolean(formik.errors.startTime),
+                    },
+                  }}
+                  value={formik.values.startTime}
+                  onChange={(newValue) => {
+                    formik.setFieldValue('startTime', newValue)
+                  }}
+                />
+                <FormHelperText
+                  error={
+                    formik.touched.startTime && Boolean(formik.errors.startTime)
+                  }
+                >
+                  {formik.touched.startTime && formik.errors.startTime}
+                </FormHelperText>
+              </FormControl>
+            </LocalizationProvider>
+            <LocalizationProvider
+              dateAdapter={AdapterDayjs}
+              adapterLocale="pt-br"
+            >
+              <FormControl size="small">
+                <TimePicker
+                  name="endTime"
+                  label="Termina às"
+                  ampm={false}
+                  slotProps={{
+                    textField: {
+                      name: 'endTime',
+                      size: 'small',
+                      type: 'text',
+                      variant: 'outlined',
+                      error:
+                        formik.touched.endTime &&
+                        Boolean(formik.errors.endTime),
+                    },
+                  }}
+                  value={formik.values.endTime}
+                  onChange={(newValue) => {
+                    formik.setFieldValue('endTime', newValue)
+                  }}
+                />
+                <FormHelperText
+                  error={
+                    formik.touched.endTime && Boolean(formik.errors.endTime)
+                  }
+                >
+                  {formik.touched.endTime && formik.errors.endTime}
+                </FormHelperText>
+              </FormControl>
+            </LocalizationProvider>
+            <FormControl fullWidth size="small">
+              <InputLabel id="repeat-select-label">Repetir Semanalmente</InputLabel>
+              <Select
+                name="repeatAs"
+                labelId="repeat-select-label"
+                id="repeat-select"
+                label="Repetir"
+                value={formik.values.repeatAs}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.repeatAs && Boolean(formik.errors.repeatAs)
+                }
+              >
+                <MenuItem value="monday">Segunda-Feira</MenuItem>
+                <MenuItem value="tuesday">Terça-Feira</MenuItem>
+                <MenuItem value="wednesday">Quarta-Feira</MenuItem>
+                <MenuItem value="thursday">Quinta-Feira</MenuItem>
+                <MenuItem value="friday">Sexta-Feira</MenuItem>
+                <MenuItem value="saturday">Sábado</MenuItem>
+                <MenuItem value="sunday">Domingo</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl component="fieldset" variant="standard">
+            <Box
+              display="flex"
+              padding="16px"
+              justifyContent="center"
+              alignItems="center"
+              gap="80px"
+              alignSelf="stretch"
+            >
+              <Button variant="contained">Excluir Tudo</Button>
+            </Box>
+            </FormControl>
+            
+          </Stack>
+        </DialogContent>
+        <DialogActions
+          sx={{
+            px: 3,
+            pb: 3,
+          }}
+        >
+          <Button variant="contained" fullWidth onClick={formik.submitForm}>
+            Salvar alterações
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
+}
+
+function Checkbox(props: CheckboxProps) {
+  return (
+    <MuiCheckbox
+      disableRipple
+      color="primary"
+      inputProps={{ 'aria-label': 'Checkbox demo' }}
+      {...props}
+    />
   )
 }
